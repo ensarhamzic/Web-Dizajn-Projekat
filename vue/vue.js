e7e2623a8c89772a11f5f046888aa58839857a0d
Ensar <ensarhamzic01@gmail.com>
2022-01-17 20:24:06 +0100

    commit

--- vue/vue.js
+++ vue/vue.js
@@ -4,60 +4,94 @@ const app = Vue.createApp({
       friends: [
         {
           id: "1",
-          name: "Ensar Hamzić",
+          name: "Clark Petersen",
           phone: "066/154-14-87",
-          email: "ensarhamzic@gmail.com",
+          email: "clarkpetersen@gmail.com",
+          image: "https://i.pravatar.cc/300?img=60",
         },
         {
           id: "2",
-          name: "Tarik Ibrahimović",
+          name: "James Nelson",
           phone: "063/124-48-72",
-          email: "tarikibrahimovic@gmail.com",
+          email: "jamesnelson@gmail.com",
+          image: "https://i.pravatar.cc/300?img=59",
         },
         {
           id: "3",
-          name: "Emir Gicić",
+          name: "Joe Davis",
           phone: "061/148-55-67",
-          email: "emirgicic@gmail.com",
+          email: "joedavis@gmail.com",
+          image: "https://i.pravatar.cc/300?img=53",
         },
         {
           id: "4",
-          name: "Ramiz Šabović",
+          name: "David Olson",
           phone: "065/177-99-25",
-          email: "ramizsabovic@yahoo.com",
+          email: "davidolson@yahoo.com",
+          image: "https://i.pravatar.cc/300?img=14",
         },
       ],
       counter: 0,
+      donateVisible: false,
+      thanks: false,
     };
   },
   methods: {
     add(num) {
-      this.counter = this.counter + num;
+      if (!this.thanks) {
+        if (this.counter == 0) {
+          this.toggleDonate();
+        }
+        if (this.counter + num <= 1000) {
+          this.counter = this.counter + num;
+        } else {
+          this.counter = 1000;
+        }
+      }
     },
     reduce(num) {
-      if (this.counter - num >= 0) {
-        this.counter = this.counter - num;
-      } else {
-        this.counter = 0;
+      if (!this.thanks) {
+        let temp = this.counter;
+        if (this.counter - num >= 0) {
+          this.counter = this.counter - num;
+        } else {
+          this.counter = 0;
+        }
+        if (this.counter == 0 && temp != 0) {
+          this.toggleDonate();
+        }
       }
     },
     donate() {
-      this.counter = 0;
+      this.toggleDonate();
+      this.toggleThanks();
+    },
+    toggleDonate() {
+      this.donateVisible = !this.donateVisible;
+    },
+    toggleThanks() {
+      this.thanks = !this.thanks;
+      setTimeout(() => {
+        this.thanks = !this.thanks;
+        this.counter = 0;
+      }, 3000);
     },
   },
 });
 
 app.component("member", {
-  props: ["name", "phone", "email"],
+  props: ["name", "phone", "email", "image"],
   template: `
   <li>
     <h2>{{ name }}</h2>
-    <ul v-if="detailsAreVisible">
-      <li><i class="fas fa-phone-alt"></i> {{ phone }}</li>
-      <li><i class="fas fa-envelope"></i>  {{ email }}</li>
+    <ul v-if="detailsAreVisible" class="inner">
+    <li><img :src='image' /></li>
+      <li><i class="fas fa-phone-alt"></i> &nbsp;&nbsp; {{ phone }}</li>
+      <li><i class="fas fa-envelope"></i> &nbsp;&nbsp; {{ email }}</li>
+      
     </ul>
     <button @click="toggleDetails()">
-      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      {{ detailsAreVisible ? 'Sakrij' : 'Prikaži' }} detalje
     </button>
     
   </li>
